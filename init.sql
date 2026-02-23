521d0ccec92ed7cb515ba354c44617e104f5f8d1
Evan Lokajaya <lokajaya414@gmail.com>
2026-02-23 12:02:00 +0700

    Update docker-compose and init SQL

--- init.sql
+++ init.sql
@@ -5,3 +5,14 @@ CREATE TABLE IF NOT EXISTS accounts (
     balance NUMERIC(19,2) NOT NULL,
     created_at TIMESTAMP NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS transactions (
+    id VARCHAR(36) PRIMARY KEY,
+    from_account_number VARCHAR(50),
+    to_account_number VARCHAR(50),
+    amount NUMERIC(19,2) NOT NULL,
+    type VARCHAR(20) NOT NULL,
+    status VARCHAR(20) NOT NULL,
+    description TEXT,
+    created_at TIMESTAMP NOT NULL
+);
